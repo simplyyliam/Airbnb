9bd00de0c506b4ddc1d4ef29afe4ebbc610f62cf
simplyyliam <simplyliam.dev@gmail.com>
2025-11-17 14:17:45 +0200

    ✨feat(hero): add hero section to the home page

--- src/conponents/pages/Home.jsx
+++ src/conponents/pages/Home.jsx
@@ -1,9 +1,34 @@
-import "./Home.css"
+import { Link } from 'react-router-dom'
+import './Home.css'
 
-export default function Home() {
-    return (
-        <div className="home-container">
-            Hello World
+export default function Home () {
+  return (
+    <div className='home-container'>
+      <div className="hero-inner">
+          <div className='hero'>
+            <div className='header'>
+              <h1>Not sure where to go? Perfect.</h1>
+              <Link>
+                <button>I'm flexible</button>
+              </Link>
+            </div>
+          </div>
+      </div>
+      <div className="next-trips">
+        <h1>Inspiration for your next trip</h1>
+        <div className="carousel">
+            <div className="card">
+                <div className="card-hero"></div>
+                <div className="card-info">
+                    <h1>Sandton City Hotel</h1>
+                    <p>53 km away</p>
+                </div>
+            </div>
         </div>
-    )
+      </div>
+      <div className="discover"></div>
+      <div className="shop-airbnb"></div>
+      <div className="questions"></div>
+    </div>
+  )
 }
--- src/conponents/ui/Searchbar.jsx
+++ src/conponents/ui/Searchbar.jsx
@@ -5,47 +5,50 @@ import { ChevronDown, Search } from 'lucide-react'
 export default function Searchbar () {
   return (
     <div id='container'>
-        <div className='search-container'>
-      <div className='locations'>
-        <div className='header'>
-          <h1>Locations</h1>
-          <p>Select Location</p>
+      <div className='search-container'>
+        <div className='locations'>
+          <div className='header'>
+            <h1>Locations</h1>
+            <p>Select Location</p>
+          </div>
+          {/* <span className='btn'>
+            <ChevronDown color='blue' />
+          </span> */}
         </div>
-        <span className='btn'>
-          <ChevronDown color='blue' />
-        </span>
-      </div>
-      <div className='check-in'>
-        <div className='header'>
-          <h1>Check in</h1>
-          <p>Add dates</p>
+        <span className='separator' />
+        <div className='check-in'>
+          <div className='header'>
+            <h1>Check in</h1>
+            <p>Add dates</p>
+          </div>
+          {/* <span className='btn'>
+            <ChevronDown color='blue' />
+          </span> */}
         </div>
-        <span className='btn'>
-          <ChevronDown color='blue' />
-        </span>
-      </div>
-      <div className='checkout'>
-        <div className='header'>
-          <h1>Check out</h1>
-          <p>Add dates</p>
+        <span className='separator' />
+        <div className='checkout'>
+          <div className='header'>
+            <h1>Check out</h1>
+            <p>Add dates</p>
+          </div>
+          {/* <span className='btn'>
+            <ChevronDown color='blue' />
+          </span> */}
         </div>
-        <span className='btn'>
-          <ChevronDown color='blue' />
-        </span>
-      </div>
-      <div className='guests'>
-        <div className='header'>
-          <h1>Guests</h1>
-          <p>Add guests</p>
+        <span className='separator' />
+        <div className='guests'>
+          <div className='header'>
+            <h1>Guests</h1>
+            <p>Add guests</p>
+          </div>
+          {/* <span className='btn'>
+            <ChevronDown color='blue' />
+          </span> */}
         </div>
-        <span className='btn'>
-          <ChevronDown color='blue' />
-        </span>
+        <div className='search'>
+          <Search />
+        </div>
       </div>
-      <div className='search'>
-        <Search />
-      </div>
-    </div>
     </div>
   )
 }
