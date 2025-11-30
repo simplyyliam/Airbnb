0c999b2c4dff7df29c5b1ad7115ef63720fcc0a7
simplyyliam <simplyliam.dev@gmail.com>
2025-11-30 17:01:52 +0200

    refactor(axios): streamline API_BASE definition for clarity

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-const API_BASE =
-  import.meta.env.MODE === "development"
-    ? "http://localhost:5000"
-    : "https://airbnb-backend-07gj.onrender.com";
+const API_BASE = import.meta.env.VITE_API_BASE_URL || 
+                 (import.meta.env.MODE === "development"
+                 ? "http://localhost:5000"
+                 : "https://airbnb-backend-07gj.onrender.com");
 
-export default axios.create({
-  baseURL: API_BASE,
-});
+export default axios.create({ baseURL: API_BASE });
